14ed0015af49d0eeca24ac572d5c7325af8d0d09
AshaduzzamanAbira <110991008+AshaduzzamanAbir@users.noreply.github.com>
2025-10-09 10:18:26 +0600

    add some file like storage service js and modify some file

--- backend/src/controllers/reels.controller.js
+++ backend/src/controllers/reels.controller.js
@@ -1,7 +1,17 @@
 const reelsModel = require("../models/Alldayreels.model");
+const storageService = require("../services/storage.service");
+const { v4: uuidv4 } = require("uuid");
 
 async function reelsCreate(req, res) {
   console.log(req.alldareels);
+  console.log(req.body);
+  console.log(req.file);
+
+  const fileUploadResult = await storageService.uploadFile(
+    req.file.buffer,
+    uuidv4()
+  );
+  console.log(fileUploadResult);
 
   res.send("reels item created");
 }
--- backend/src/routes/reels.routes.js
+++ backend/src/routes/reels.routes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
 const reelsController = require("../controllers/reels.controller");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer(
+  { storage: multer.memoryStorage() } // Store files in memory as Buffer objects
+);
 
 // POSR/api/reels
 router.post(
   "/",
   authMiddleware.authalldayreelsMiddleware,
+  upload.single("video"),
   reelsController.reelsCreate
 );
 
